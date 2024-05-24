13082f735ffe8e48ea5bc7fcf6a88183c094b76c
Bruno Reis <bruno.reiis15@gmail.com>
2024-05-24 19:00:06 -0300

    feature: database file

--- src/server.js
+++ src/server.js
@@ -1,37 +1,31 @@
 import http from "node:http";
-import { Database } from "./database";
+import { Database } from "./database.js";
+import { json } from "./middlewares/json.js";
 
-const database = new Database
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = []
-
-  for await(const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = Buffer.concat(buffers).toString()
-  } catch{
-    req.body = null
-  }
+  await json(req, res);
 
   if (method == "GET" && url == "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    const users = database.select("users");
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method == "POST" && url == "/users") {
-    const { name, email } = req.body
+    const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
+
+    database.insert("users", user);
+
     return res.writeHead().end();
   }
 

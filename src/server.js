c2b2dbf40dbe08dcc2d4034aabd3cc16d183a2c8
Bruno Reis <bruno.reiis15@gmail.com>
2024-05-24 17:05:26 -0300

    feature: add HTTP response

--- src/server.js
+++ src/server.js
@@ -1,26 +1,28 @@
-import http from 'node:http'
+import http from "node:http";
 
-const users = []
+const users = [];
 
 const server = http.createServer((req, res) => {
-    const { method, url } = req
+  const { method, url } = req;
 
-    if (method == 'GET' && url == '/users'){
-        return res.end(JSON.stringify(users))
-    }
+  if (method == "GET" && url == "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
 
-    if (method == 'POST' && url == '/users'){
-        users.push({
-            id: 1,
-            name: 'Jhon Doe',
-            email: 'jhondoe@email.com'
-        })
-        return res.end('Criação de usuario')
-    }
+  if (method == "POST" && url == "/users") {
+    users.push({
+      id: 1,
+      name: "Jhon Doe",
+      email: "jhondoe@email.com",
+    });
+    return res.writeHead().end();
+  }
 
-    console.log(method,url)
+  console.log(method, url);
 
-    return res.end('Hello World')
-})
+  return res.writeHead(404).end();
+});
 
-server.listen(3333)
+server.listen(3333);
